0d3b589c3d10a966f9d03fc21484f2d61e2d42df
Radenbudi01 <94732449+Radenbudi01@users.noreply.github.com>
2021-11-26 19:26:51 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '2133290397:AAHIgB1ZusJ-m2-TluhcnWMfZQNwfRPMk1U';
+const token = '2137801091:AAGnAjM265jvx4Ls8WNI-hroTQwHq-cB4U0';
 const bot = new TelegramBot(token, {polling: true});
 
 
